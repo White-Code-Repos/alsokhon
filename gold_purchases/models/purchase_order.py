0db8e71c2f1fa601b3687add905fae2026bf3f13
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-02-02 14:47:11 +0200

    back to old state

--- gold_purchases/models/purchase_order.py
+++ gold_purchases/models/purchase_order.py
@@ -27,17 +27,10 @@ class assemblyDescriptionGold(models.Model):
             elif self.product_id.gold and not self.product_id.scrap:
                 self.purity = self.purity_id.purity
                 self.pure_weight = self.gross_weight * (self.purity / 1000)
-    @api.onchange('purity')
-    def change_hall(self):
-        if self.purity:
-            _logger.info(self.purity)
-            _logger.info(self.gross_weight)
-            _logger.info(self.pure_weight)
-            self.pure_weight = self.gross_weight * (self.purity / 1000)
-            _logger.info(self.pure_weight)
+
     our_stock = fields.Boolean(default=False)
     purity = fields.Float(digits=(16,3))
-    # polish_rhodium = fields.Float('Polish & Rhodium',digits=(16,3))
+    polish_rhodium = fields.Float('Polish & Rhodium',digits=(16,3))
     purchase_id_gold = fields.Many2one('purchase.order')
 
 class assemblyDescriptionDiamond(models.Model):
@@ -194,11 +187,6 @@ class assemblyComponentsDiamond(models.Model):
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-
-    assembly_gold_main_make = fields.Float('Gold Making', digits=(16,3))
-    assembly_gold_main_par = fields.Float('Polish & Rhodium', digits=(16,3))
-
-
     state = fields.Selection([
         ('draft', 'RFQ'),
         ('processing', 'In Progress'),
@@ -606,10 +594,6 @@ class PurchaseOrder(models.Model):
         # self.write({'assembly_description_diamond':stone_description_lines})
         self.state = 'receive'
 
-    def draft_review_assembly(self):
-        self.update_po_line()
-        self.write({'state':'review'})
-
     def review_assembly(self):
         stone_description_lines = []
         for line in self.assembly_diamond_ids:
@@ -647,7 +631,7 @@ class PurchaseOrder(models.Model):
                 'purity_id':old_line.purity_id.id or False,
                 'purity':old_line.purity,
                 'pure_weight':old_line.pure_weight,
-                # 'polish_rhodium':old_line.polish_rhodium,
+                'polish_rhodium':old_line.polish_rhodium,
                 'our_stock':True
                 }))
             else:
@@ -682,7 +666,7 @@ class PurchaseOrder(models.Model):
         for line in self.assembly_description_gold:
             total_gross += line.gross_weight
             # total_make += line.make_rate
-            # total_r_p += line.polish_rhodium
+            total_r_p += line.polish_rhodium
         pol = self.env['purchase.order.line'].search([('order_id','=',self.id)])
         if pol and pol[0]:
             print(total_stones_carat)
@@ -691,24 +675,13 @@ class PurchaseOrder(models.Model):
             print(self.assembly_description_gold[0].purity_id.id)
             print(total_r_p)
             print(total_stones_price)
-            if self.assembly_description_gold[0].purity != self.assembly_description_gold[0].purity_id.purity:
-                pol[0].write({
-                'carat':total_stones_carat,
-                'd_make_value':total_stones_labor,
-                'purity_hall':self.assembly_description_gold[0].purity,
-                'gross_wt':total_gross,
-                'purity_id':self.assembly_description_gold[0].purity_id.id,
-                # 'polish_rhodium':total_r_p,
-                })
-            else:
-                pol[0].write({
-                'carat':total_stones_carat,
-                'd_make_value':total_stones_labor,
-                # 'purity_hall':self.assembly_description_gold[0].purity,
-                'gross_wt':total_gross,
-                'purity_id':self.assembly_description_gold[0].purity_id.id,
-                # 'polish_rhodium':total_r_p,
-                })
+            pol[0].write({
+            'carat':total_stones_carat,
+            'd_make_value':total_stones_labor,
+            'gross_wt':total_gross,
+            'purity_id':self.assembly_description_gold[0].purity_id.id,
+            'polish_rhodium':total_r_p,
+            })
             if self.assembly_no_giving:
                 pol[0].write({
                 'price_unit':pol[0].price_unit+total_stones_price+pol[0].gold_value,
@@ -1283,7 +1256,7 @@ class PurchaseOrderLine(models.Model):
     make_rate = fields.Monetary('Make Rate/G', digits=(16, 3))
     make_value = fields.Monetary('Make Value', compute='_get_gold_rate',
                                  digits=(16, 3), default=0.00)
-    # polish_rhodium = fields.Float('Polish & Rhodium',digits=(16,3))
+    polish_rhodium = fields.Float('Polish & Rhodium',digits=(16,3))
 
     gold_rate = fields.Float('Gold Rate/G', compute='_get_gold_rate',
                              digits=(16, 3))
@@ -1404,8 +1377,6 @@ class PurchaseOrderLine(models.Model):
             #              0.00) + new_pure_wt
             if rec.order_id.diamond:
                 rec.make_value = 0.00
-            elif rec.order_id.assembly:
-                rec.make_value = rec.product_qty * rec.gross_wt * rec.make_rate + rec.order_id.assembly_gold_main_make + rec.order_id.assembly_gold_main_par
             else:
                 if rec.product_id.categ_id.is_scrap:
                     rec.make_value = rec.gross_wt * rec.make_rate
@@ -1471,7 +1442,7 @@ class PurchaseOrderLine(models.Model):
             elif line.product_id.assembly:
                 vals = line._prepare_compute_all_values()
                 taxes = line.taxes_id.compute_all(
-                    vals['price_unit'],
+                    vals['price_unit']+line.polish_rhodium,
                     vals['currency_id'],
                     1,
                     vals['product'],
@@ -1823,8 +1794,8 @@ class PurchaseOrderLine(models.Model):
                     'make_value': self.make_value,
                     'd_make_value': self.d_make_value,
                     'gold_value': self.gold_value,
-                    # 'polish_rhodium': self.polish_rhodium,
-                    'price_unit': self.price_unit,
+                    'polish_rhodium': self.polish_rhodium,
+                    'price_unit': self.price_unit + self.polish_rhodium,
                     # 'price_subtotal': self.price_unit * self.product_qty,
                     'discount': self.discount,
                     })
@@ -1935,8 +1906,8 @@ class PurchaseOrderLine(models.Model):
                     'make_value': self.make_value,
                     'd_make_value': self.d_make_value,
                     'gold_value': self.gold_value,
-                    # 'polish_rhodium': self.polish_rhodium,
-                    'price_unit': self.price_unit,
+                    'polish_rhodium': self.polish_rhodium,
+                    'price_unit': self.price_unit + self.polish_rhodium,
                     # 'price_subtotal': self.price_unit * self.product_qty,
                     'discount': self.discount,
                     })
