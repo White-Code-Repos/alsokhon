61b349af4041cc794793dacbeb0d120c13fb4a1a
aelmahdi159 <aelmahdi@white-code.co.uk>
2021-02-14 14:55:22 +0200

    Update pos.js

--- pos_unfixed/static/src/js/pos.js
+++ pos_unfixed/static/src/js/pos.js
@@ -355,7 +355,8 @@ odoo.define('pos_unfixed.pos', function(require){
         console.log(order.get_due_converted_fix());
         console.log(order.get_total_with_tax() , order.get_total_paid());
 
-console.log(!order.get_due_converted_fix()<1);
+        console.log(!order.get_due_converted_fix()<1);
+        console.log(order.get_due_converted_fix()<1);
         if (!order.get_due_converted_fix()<1){
           self.gui.show_popup('error',{
           	'title': _t('Order Not Paid'),
