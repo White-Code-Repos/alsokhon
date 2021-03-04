6312571bcda7c48a5196d22d6fe9587bfc5757dc
Abdulrahman Hassan <awarda@white-code.co.uk>
2021-03-04 10:13:22 +0200

    Update purchase_order.py

--- po_gold_form/models/purchase_order.py
+++ po_gold_form/models/purchase_order.py
@@ -13,12 +13,12 @@ class PurchaseOrder(models.Model):
     hide_order_gold_rate = fields.Boolean(compute="_compute_hide_order_gold_rate")
     def _compute_hide_order_gold_rate(self):
         for this in self:
-            _logger.info("this.gold,this.assembly,this.is_unfixed")
             _logger.info((this.gold == False and this.assembly == False) or this.is_unfixed == True)
-            _logger.info("this.gold,this.assembly,this.is_unfixed")
             if (this.gold == False and this.assembly == False) or this.is_unfixed == True:
+                _logger.info("True")
                 this.hide_order_gold_rate == True
             else:
+                _logger.info("False")
                 this.hide_order_gold_rate == False
 
     @api.onchange('currency_id', 'date_order', 'order_type')
